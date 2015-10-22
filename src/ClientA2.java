bc60a1bea0531771f84c5371868d77d6c4b31f71
John Claro <jkrclaro@gmail.com>
2015-10-22 22:44:36 +0100

    Implement minimum viable product

--- src/ClientA2.java
+++ src/ClientA2.java
@@ -1,34 +1,166 @@
 import java.net.Socket;
-import java.util.Scanner;
-import java.io.DataInputStream;
-import java.io.InputStreamReader;
 import java.io.DataOutputStream;
-import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.DataInputStream;
+
+//GUI
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import java.awt.Container;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.BorderFactory;
 
 public class ClientA2
-{
-	public static void main(String[] args)
-	{
+{	
+	public void login(Socket socket, DataOutputStream toServer, DataInputStream fromServer)
+	{	
 		try
-		{	
-			System.out.println("What is your name?: ");
-			Scanner clientInput = new Scanner(System.in);
-			String clientName = clientInput.next();
-			Socket socket = new Socket("localhost", 2994);
-			
-			DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
-			
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-			
+		{				
 			while (true)
-			{
-				System.out.println("Enter a message: ");
-				toServer.writeUTF(clientName + ": " + bufferedReader.readLine());
+			{				
+				String input = JOptionPane.showInputDialog(null, "Account Number");
+				toServer.writeUTF("AccountNumber" + "," + input);
+				String fullName = fromServer.readUTF();
+				
+				// TODO: Handle user input when OK button is clicked and empty
+				if (input != null)
+				{
+					if (fullName != "ABCDEFGHJKLMNOPQRSTUVWXYZ")
+					{
+						JOptionPane.showMessageDialog(null, "Welcome, " + fullName);
+						break;
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "Sorry, you are not a registered client", "Warning", JOptionPane.WARNING_MESSAGE);
+					}
+				}
+				else
+				{
+					socket.close();
+					System.exit(0);
+				}
 			}
 		}
 		catch(Exception e)
 		{
 			System.out.println("Cannot connect to the server");
+		}
+	}
+	
+	public void send(Socket socket, DataOutputStream toServer, DataInputStream fromServer)
+	{
+		JFrame frame = new JFrame("Client");
+		JPanel panel = new JPanel();
+		
+		frame.setSize(500, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setLayout(new BorderLayout());
+		
+		JTextArea textArea = new JTextArea();
+		
+		Container c = frame.getContentPane();
+		c.add(textArea, BorderLayout.CENTER);
+		c.add(panel, BorderLayout.NORTH);
+		
+		// JPanel + GridBagConstraints
+		
+		panel.setBorder(BorderFactory.createTitledBorder("Client"));
+		
+		JLabel annualInterestRateLabel = new JLabel("Annual Interest Rate: ");
+		JLabel numberOfYearsLabel = new JLabel("Number Of Years: ");
+		JLabel loanAmountLabel = new JLabel("Loan Amount: ");
+		
+		JTextField annualInterestRateField = new JTextField(10);
+		JTextField numberOfYearsField = new JTextField(10);
+		JTextField loanAmountField = new JTextField(10);
+		
+		JButton submitButton = new JButton("Submit");
+		submitButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				String annualInterestRate = annualInterestRateField.getText();
+				String numberOfYears = numberOfYearsField.getText();
+				String loanAmount = loanAmountField.getText();
+				
+				try 
+				{
+					toServer.writeUTF(annualInterestRate + "," + numberOfYears + "," + loanAmount);
+					textArea.append(fromServer.readUTF() + "\n");
+				} 
+				catch (IOException error) {
+					System.out.println(error);
+				}
+			}
+			
+		});
+		
+		panel.setLayout(new GridBagLayout());
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		
+		// First Column
+		gc.gridx = 0;
+		gc.gridy = 0;
+		panel.add(annualInterestRateLabel, gc);
+		
+		gc.gridx = 0;
+		gc.gridy = 1;
+		panel.add(numberOfYearsLabel, gc);
+		
+		gc.gridx = 0;
+		gc.gridy = 2;
+		panel.add(loanAmountLabel, gc);
+		
+		// Second Column
+		gc.gridx = 1;
+		gc.gridy = 0;
+		panel.add(annualInterestRateField, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 1;
+		panel.add(numberOfYearsField, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 2;
+		panel.add(loanAmountField, gc);
+		
+		gc.gridx = 2;
+		gc.gridy = 2;
+		gc.gridheight = 100;
+		panel.add(submitButton, gc);
+		
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		try
+		{
+			Socket socket = new Socket("localhost", 2994);
+			DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
+			DataInputStream fromServer = new DataInputStream(socket.getInputStream());
+			
+			ClientA2 clientA2 = new ClientA2();
+			clientA2.login(socket, toServer, fromServer);
+			clientA2.send(socket, toServer, fromServer);
+		}
+		catch (Exception e)
+		{
+			JOptionPane.showMessageDialog(null, "Unable to connect to server", "Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 }
--- src/MultiThreadedServerA2.java
+++ src/MultiThreadedServerA2.java
@@ -1,27 +1,37 @@
 import java.net.Socket;
-import java.io.DataInputStream;
-import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Date;
 
 class MultiThreadedServerA2
 {	
 	public static void main(String[] args)
 	{
 		final int portNumber = 2994;
+		
 		try
 		{
 			ServerSocket serverSocket = new ServerSocket(portNumber);
-			System.out.println("Server is currently listening at port " + portNumber);
-			
+			System.out.println("Server started at " + new Date() + "\n");
 			while (true)
 			{
 				try
 				{
-					Socket clientSocket = serverSocket.accept();
-					Thread thread = new Thread(new MyClient(clientSocket));
-					thread.start();
-					System.out.println("Number of clients currently connected: " + (Thread.activeCount() - 1));
+					Socket socket = serverSocket.accept();
+					MyClient myClient = new MyClient(socket);
+					myClient.start();
 				}
 				catch (Exception e)
 				{
@@ -36,28 +46,109 @@ class MultiThreadedServerA2
 	}
 }
 
-class MyClient implements Runnable
+class MyClient extends Thread
 {	
 	private Socket socket;
+	private String hostName;
+	private String ipAddress;
+	private DataOutputStream toClient;
+	private DataInputStream fromClient;
+	private long clientID;
 
-	public MyClient(Socket socket)
+	public MyClient(Socket socket) throws IOException
 	{	
 		this.socket = socket;
+		this.hostName = socket.getInetAddress().getHostName();
+		this.ipAddress = socket.getInetAddress().getHostAddress();
+		this.toClient = new DataOutputStream(socket.getOutputStream());
+		this.fromClient = new DataInputStream(socket.getInputStream());
+		this.clientID = Thread.activeCount();
 	}
 	
 	public void run()
 	{
+		System.out.println("Client " + clientID + " [Hostname: " + hostName + " , " + "IP: " + ipAddress +"]");
 		try
 		{
-			DataInputStream fromClient = new DataInputStream(socket.getInputStream());
 			while (true)
 			{
-				System.out.println(fromClient.readUTF());
+				String fromClientMessage = fromClient.readUTF();
+				
+				if (fromClientMessage.contains("AccountNumber"))
+				{
+					List<String> clientData = Arrays.asList(fromClientMessage.split(","));
+					
+					String fullName = checkDatabase(clientData.get(1));
+					toClient.writeUTF(fullName);
+				}
+				else
+				{
+					List<String> clientData = Arrays.asList(fromClientMessage.split(","));
+					
+					Double annualInterestRate = Double.parseDouble(clientData.get(0));
+					Double numberOfYears = Double.parseDouble(clientData.get(1));
+					Double loanAmount = Double.parseDouble(clientData.get(2));
+					
+					String monthlyPayment = calculateMonthlyPayment(annualInterestRate, numberOfYears, loanAmount);
+					
+					toClient.writeUTF(monthlyPayment);
+				}
 			}
 		}
 		catch (Exception e)
 		{
-			System.out.println("Error in retrieving message from client");
+			System.out.println("A client has disconnected");
 		}
 	}
+	
+	public String checkDatabase(String accountNumber) throws SQLException
+	{
+		try
+		{
+			// Connect to database
+			Properties connectionProperties = new Properties();
+			connectionProperties.put("user", "root");
+			connectionProperties.put("password", "root");
+			String serverLink = "jdbc:mysql://localhost:3306/BankDatabase";
+	        Connection connection = DriverManager.getConnection(serverLink, connectionProperties);
+	        
+	        Statement statement = connection.createStatement();
+	        statement.executeQuery("SELECT * FROM RegisteredApplicants WHERE AccountNum = " + accountNumber);
+	        
+	        // Check what database returns
+	        ResultSet resultSet = statement.getResultSet();
+	        while (resultSet.next())
+	        {	
+	        	if (Integer.parseInt(accountNumber) == resultSet.getInt("AccountNum"))
+	        	{
+	        		String firstName = resultSet.getString("FirstName");
+	        		String lastName = resultSet.getString("LastName");
+	        		return firstName + " " + lastName;
+	        	}
+	        	else
+	        	{
+	        		return "ABCDEFGHJKLMNOP" + "QRSTUVWXYZ";
+	        	}
+	        }
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e);
+		}
+		
+		return "ABCDEFGHJKLMNOP" + "QRSTUVWXYZ";
+	}
+	
+	public String calculateMonthlyPayment(double annualInterestRate, double numberOfYears, double loanAmount)
+	{
+		annualInterestRate /= 100.0;
+		double monthlyRate = annualInterestRate / 12.0;
+		double termInMonths = numberOfYears * 12;
+		double monthlyPaymentNumber = (loanAmount * monthlyRate) / (1-Math.pow(1+monthlyRate, -termInMonths));
+		String monthlyPaymentString = String.valueOf(monthlyPaymentNumber);
+		
+		return monthlyPaymentString;
+	}
+	
+	
 }
